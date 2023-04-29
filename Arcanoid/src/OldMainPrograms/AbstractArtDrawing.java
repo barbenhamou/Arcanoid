e6af9a5ad8e7c3541cc69c38d76b3ee0d5708780
barbenhamou <barbenhamou2006@gmail.com>
2023-04-29 18:41:08 +0300

    initial

---
+++ Arcanoid/src/OldMainPrograms/AbstractArtDrawing.java
@@ -0,0 +1,72 @@
+package OldMainPrograms;
+
+import biuoop.GUI;
+import biuoop.DrawSurface;
+import AbstractShapes.Line;
+import AbstractShapes.Point;
+import java.util.Random;
+import java.awt.Color;
+
+/**
+ * Sort of tester class, GUI class, all the graphics of geometry.
+ */
+public class AbstractArtDrawing {
+
+    static final int WIDTH = 400, HEIGHT = 300, AMOUNT = 10, R = 3;
+
+    /**
+     * Name: lineDrawingFrame<br>
+     * Action :<p>
+     * Initialize screen, drawing 10 lines,
+     * color their middle points and their intersection points.
+     * Using the biuoop library.
+     * </p>
+     */
+    public void lineDrawingFrame() {
+        Random rand = new Random();
+        Line[] lines = new Line[AMOUNT];
+        GUI screen = new GUI("AbstractShapes.Line Drawing Frame", WIDTH, HEIGHT);
+        DrawSurface tool = screen.getDrawSurface();
+        int x1, y1, x2, y2;
+        Point middle, intersection;
+
+        for (int i = 0; i < 10; ++i) {
+            x1 = rand.nextInt(WIDTH) + 1; // get "integer" in range 1-800
+            x2 = rand.nextInt(WIDTH) + 1; // get "integer" in range 1-800
+            y2 = rand.nextInt(HEIGHT) + 1; // get "integer" in range 1-800
+            y1 = rand.nextInt(HEIGHT) + 1; // get "integer" in range 1-800
+            lines[i] = new Line(x1, y1, x2, y2);
+
+            tool.setColor(Color.BLACK);
+            tool.drawLine(x1, y1, x2, y2);
+
+            tool.setColor(Color.BLUE);
+            middle = lines[i].middle();
+
+            tool.fillCircle((int) (Math.round(middle.getX())),
+                    (int) (Math.round(middle.getY())), R);
+        }
+
+        for (Line line : lines) {
+            for (Line line1 : lines) {
+                intersection = line.intersectionWith(line1);
+                if (intersection == null || line.equals(line1)) {
+                    continue;
+                }
+                tool.setColor(Color.RED);
+                tool.fillCircle((int) (Math.round(intersection.getX())),
+                        (int) (Math.round(intersection.getY())), R);
+            }
+        }
+        screen.show(tool);
+    }
+
+    /**
+     * Running the program and calling the function that initializes everything.
+     * @param args - the cmd arguments
+     */
+    public static void main(String[] args) {
+        AbstractArtDrawing example = new AbstractArtDrawing();
+        example.lineDrawingFrame();
+    }
+}
---
+++ Arcanoid/src/Utils/Velocity.java
@@ -0,0 +1,113 @@
+package Utils;
+
+import AbstractShapes.Point;
+
+/**
+ * Utils.Velocity class, helper to the animation and physics of the ball.
+ */
+public class Velocity {
+    private double dx;
+    private double dy;
+
+    /**
+     * Constructor.
+     *
+     * @param dx
+     * @param dy
+     */
+    public Velocity(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    /**
+     * Getter for x-axis speed.
+     *
+     * @return x-axis speed
+     */
+    public double getDx() {
+        return dx;
+    }
+
+    /**
+     * Getter for y-axis speed.
+     *
+     * @return y-axis speed
+     */
+    public double getDy() {
+        return dy;
+    }
+
+    /**
+     * Getter for speed.
+     *
+     * @return speed
+     */
+    public double getSpeed() {
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    /**
+     * Setter for x-axis speed.
+     *
+     * @param dx
+     */
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    /**
+     * Setter for y-axis speed.
+     *
+     * @param dy
+     */
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    /**
+     * Applier dx and dy.
+     *
+     * @param p
+     * @return the new point after applying speed upon her
+     */
+    public Point applyToPoint(Point p) {
+        return new Point(p.getX() + dx, p.getY() + dy);
+    }
+
+    /**
+     * Applier dx.
+     *
+     * @param p
+     * @return the new point after applying dx upon her
+     */
+    public Point applyXToPoint(Point p) {
+        return new Point(p.getX() + dx, p.getY());
+    }
+
+    /**
+     * Applier dy.
+     *
+     * @param p
+     * @return the new point after applying dy upon her
+     */
+    public Point applyYToPoint(Point p) {
+        return new Point(p.getX(), p.getY() + dy);
+    }
+
+    /**
+     * Getter for the velocity by speed and angle in degrees.
+     *
+     * @param speed
+     * @param angle
+     * @return the velocity using angle and speed
+     */
+    public static Velocity fromAngleAndSpeed(double angle, double speed) {
+        double dx = Math.cos(Math.toRadians(angle)) * speed;
+        double dy = Math.sin(Math.toRadians(angle)) * speed;
+
+        return new Velocity(dx, dy);
+    }
+
+
+}
